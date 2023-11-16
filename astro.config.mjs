ed471b173f69b8f3e740361d071cb90db7b1e6b0
yCroma <58209438+yCroma@users.noreply.github.com>
2023-11-16 22:59:25 +0900

    base path の末尾にスラッシュを追加
    commit: aa58602 のように、baseタグでルートを設定するためには、
    baseの文字列の末尾をスラッシュで閉じる必要があった。
    
    参考discussion:
    - [Add a way to access base option from .astro component (such as
    Astro.base) #292](https://github.com/withastro/roadmap/discussions
    /292)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yCroma.github.io',
-  base: '/AstroTutorial',
+  base: '/AstroTutorial/',
 });
